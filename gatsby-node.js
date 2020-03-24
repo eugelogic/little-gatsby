67fd2319cd4934ab30dc1232700d30a1da48847c
Eugene Molari <eugelogic@gmail.com>
2020-03-24 14:45:24 +0000

    Implement the createPages and createPage APIs

---
+++ gatsby-node.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+exports.createPages = async ({
+    graphql,
+    actions: { createPage },
+    reporter
+}) => {
+    const postsQuery = await graphql(`
+        query {
+            allMdx {
+                nodes {
+                    id
+                    frontmatter {
+                    slug
+                    }
+                }
+            }
+        }
+    `)
+
+    if (postsQuery.errors) {
+        reporter.panicOnBuild(`Unable to create posts.`)
+        return
+    }
+
+    const posts = postsQuery.data.allMdx.nodes
+    posts.forEach(post => {
+        createPage({
+            path: `/blog/${post.frontmatter.slug}`,
+            component: path.resolve(`./src/templates/post.js`)
+        })
+    })
+}
---
+++ src/templates/post.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PostTemplate = () => (
+    <>
+        <h1>Hard Coded Post Title</h1>
+        <p>Post blurb hadrcoded ...</p>
+    </>
+)
+
+export default PostTemplate
