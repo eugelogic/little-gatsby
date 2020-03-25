d5903d801328d07045218c5592ce2fa3d7fb0861
Eugene Molari <eugelogic@gmail.com>
2020-03-25 17:11:00 +0000

    Add blog feed to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,48 @@
-import React from "react"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../components/layout'
 
-export default () => <div>Hello world!</div>
+import '../styles/global.css'
+
+export const query = graphql`
+ {
+    allMdx(sort: {order: DESC, fields: frontmatter___date}) {
+      nodes {
+        id
+        frontmatter {
+          tags
+          title
+          slug
+          date(formatString: "dddd DD MMMM YYYY")
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+const BlogIndex = ({ data }) => {
+    const posts = data.allMdx.nodes
+    return (
+        <Layout>
+            {posts.map(post => (
+                <article key={post.id}>
+                    {post.frontmatter.tags && post.frontmatter.tags.length ? (
+                    <ul className="tags-list">
+                    {post.frontmatter.tags.map(tag => (
+                        <li key={tag}>{tag}</li>
+                    ))}
+                    </ul>
+                    ) : null}
+                    <h2>
+                        <Link to={`/blog/${post.frontmatter.slug}`}>{post.frontmatter.title}</Link>
+                    </h2>
+                    <time>{post.frontmatter.date}</time>
+                    <p>{post.frontmatter.excerpt}</p>
+                </article>
+            ))}
+        </Layout>
+    )
+}
+
+export default BlogIndex
